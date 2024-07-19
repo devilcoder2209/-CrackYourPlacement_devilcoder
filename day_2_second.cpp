a7a6de608221519be1ef5fd73f0be5f0a9709f3e
devilcoder2209 <81303656+devilcoder2209@users.noreply.github.com>
2024-07-19 16:42:32 +0530

    Add files via upload

---
+++ day_2_second.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+
+        int col0 = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) { col0 = 0; }
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+            if (col0 == 0) matrix[i][0] = 0;
+        }
+    }
+};
+
+int main() {
+    int n, m;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    cout << "Enter the number of columns: ";
+    cin >> m;
+
+    vector<vector<int>> matrix(n, vector<int>(m));
+    cout << "Enter the elements of the matrix:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    Solution sol;
+    sol.setZeroes(matrix);
+
+    cout << "Modified matrix:" << endl;
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[0].size(); j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
